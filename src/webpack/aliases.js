b873cadc892e0a7c83deeef84cd8f69b9d9e9211
kartamyshev <kartamyshev.k@yandex.ru>
2016-03-07 15:10:56 +0300

    removed src folder

--- src/webpack/aliases.js
+++
@@ -1,8 +0,0 @@
-module.exports = function aliases(dir) {
-	return {
-    	Css: dir + '/css',
-    	Mixins: dir + '/mixins',
-    	Widgets: dir + '/components/widgets',
-    	Components: dir + '/components'
-	};
-};
--- src/webpack/deps.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	React: 'react',
-	ReactDOM: 'react-dom',
-    "_": 'lodash',
-    Utils: 'utils',
-    Please: 'please-ajax'
-};
